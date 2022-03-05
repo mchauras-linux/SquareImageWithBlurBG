168d159c77ca23e624938bcb0fbf9902bd20bf02
Mukesh Kumar Chaurasiya <mukeshmike9@gmail.com>
2022-03-05 21:01:54 +0530

    Necessary checks added

--- main.py
+++ main.py
@@ -11,15 +11,27 @@ def create_path(dirToBeCreated):
             if exc.errno != errno.EEXIST:
                 raise
 
+if(len(sys.argv) < 2):
+    print("Please provide Path to a folder...!!!")
+    exit()
+
+if(os.path.isdir(sys.argv[1]) == False):
+    print("Please Provide path to a directory containing images...!!!")
+    exit()
+
 src_dir = sys.argv[1]
 src_dir = os.path.abspath(src_dir)
+
 dest_dir = src_dir + "_Squared"
 
 for root, dirs, files in os.walk(src_dir):
     for file in files:
         src_file_path = os.path.abspath(os.path.join(root, file))
         dest_file_path  = src_file_path.replace(src_dir, dest_dir)
-        create_path(dest_file_path)
-        img = ImageLib(src_file_path).get_squared_image()
-        img.save(dest_file_path)
-        print(file +": DONE")
+        try:
+            img = ImageLib(src_file_path).get_squared_image()
+            create_path(dest_file_path)
+            img.save(dest_file_path)
+            print(file +": DONE")
+        except Exception as e:
+            print(e)
