7545f466be7bd05725ece62bfc05c8d95808c5be
Mukesh Kumar Chaurasiya <mukeshmike9@gmail.com>
2022-03-05 15:58:46 +0530

    Coordinate for Image pasting corrected

--- ImageLib.py
+++ ImageLib.py
@@ -4,6 +4,9 @@ import os
 
 class ImageLib:
 
+    BLUR_LEVEL  = 10
+    EDGE_REMOVAL_FACTOR = 0.08
+
     def __init__(self, path):
         abs_path = os.path.abspath(path)
         self.img = Image.open(abs_path)
@@ -32,8 +35,11 @@ class ImageLib:
         for i in range(level):
             self.blur_image = self.blur_image.filter(ImageFilter.BLUR)
         #To remove edges which is not perfectly blurred
-        crop_factor = self.get_max_dimension() * 0.08
+        crop_factor = self.get_max_dimension() * ImageLib.EDGE_REMOVAL_FACTOR
         self.blur_image = self.blur_image.crop((crop_factor, crop_factor, self.blur_image.width - crop_factor, self.blur_image.height - crop_factor))
+
+        #As we have removed Edges, we need to resize the image to original size
+        self.blur_image = self.blur_image.resize((int(self.blur_image.width + (crop_factor * 2)), int(self.blur_image.height + (crop_factor * 2))))
         return self.blur_image
 
     def show(self):
@@ -71,24 +77,22 @@ class ImageLib:
         #print(f"Original Width: {src_img.width}\nOriginal Height: {src_img.height}\nNew Width: {width}\nNew Height: {height}\n")
         resized_img = src_img.resize((int(width), int(height)))
         return ImageLib.crop_to_square(resized_img)
-
     
     def __create_blurry_square_background(self) -> None:
-        self.blurry_bg = ImageLib.get_cropped_square_image(self.get_blurred_image(100))
+        self.blurry_bg = ImageLib.get_cropped_square_image(self.get_blurred_image(ImageLib.BLUR_LEVEL))
 
     def get_squared_image(self) -> Image:
         self.__create_blurry_square_background()
         src_img = self.img
-        if(src_img.width > src_img.height):
-            width = src_img.height
+        if(src_img.width < src_img.height):
             margin = (self.blurry_bg.width - src_img.width) / 2
-            left = margin
-            top = 0
-        else:
-            height = src_img.width
-            margin = (src_img.height - height) / 2
             left = 0
             top = margin
+        else:
+            margin = (self.blurry_bg.height - src_img.height) / 2
+            left = margin
+            top = 0
+
         coordinate = (int(top), int(left))
         self.blurry_bg.paste(self.img, coordinate)
         return self.blurry_bg
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from ImageLib import ImageLib
 
-img = ImageLib("C:/Users/mukes/Pictures/Manisha Photos/attachments (2)/Snapchat-2000564964.jpg")
+img = ImageLib("C:/Users/mukes/Pictures/Manisha Photos/attachments (2)/Snapchat-2074536120.jpg")
 
-print(img.get_max_dimension())
-
-img.get_squared_image().show()
+img.get_squared_image().show("Final")
